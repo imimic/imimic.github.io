2d3194e8d9f4a38478f4622063e6418d90931305
MIMIC <127608004+imimic@users.noreply.github.com>
2026-05-11 02:57:40 +0800

    Update install_nginx_dmit-t1.sh

--- nginx/install_nginx_dmit-t1.sh
+++ nginx/install_nginx_dmit-t1.sh
@@ -43,23 +43,20 @@ pid       /var/run/nginx.pid;
 
 events {
     worker_connections 1024;
-    multi_accept on;
-    use epoll;
 }
 
 stream {
     resolver 8.8.8.8 8.8.4.4 valid=60s ipv6=off;
 
     map $remote_addr $target_backend {
-        default hkt.112788.xyz:4443;
+        default hkt.hkg.112788.xyz:4443;
     }
 
     server {
-        listen 443 reuseport;
+        listen 443;
         ssl_preread on;
-        tcp_nodelay on;
         proxy_connect_timeout 5s;
-        proxy_timeout 3600s;
+        proxy_socket_keepalive on;
         proxy_pass $target_backend;
     }
 }
